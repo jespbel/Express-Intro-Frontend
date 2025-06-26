0a630eea4837cf27bb81242436e9c6eab1169d5d
jespbel <jmbelaso@buffalo.edu>
2025-06-26 14:38:15 -0400

    Changed url back

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
 
   async function fetchAllTasks() {
     try {
-      const response = await axios.get("http://localhost:8080/tasks");
+      const response = await axios.get("http://localhost:8080/api/tasks");
       setTasks(response.data);
       console.log(tasks)
     } catch (error) {
